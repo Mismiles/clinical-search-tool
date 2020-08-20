6ce20cd43082306f56129f18d75cf72f7d417d1a
Msmiles <muhammadirfan123@hotmail.com>
2020-08-20 09:45:14 +0000

    changed css body background to no-repeat

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+import bcrypt
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
@@ -11,11 +13,43 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "PharmacyLinks"
 app.config["MONGO_URI"] = os.getenv("MONGODB_LIST")
 
+flask_bcrypt = Bcrypt(app)
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
     return render_template("index.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('login.html'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 @app.route("/results")
 def results():
@@ -24,14 +58,6 @@ def results():
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
 @app.route("/resourcelist")
 def resourcelist():
@@ -120,3 +146,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+    app.secret_key = 'mysecret'
